fb1b21f1339edfdcb96c76506d7ee5d7401650d3
Lara Sanders <larasanders@gmail.com>
2018-07-25 21:32:28 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,6 +1,6 @@
 function hideWhenClicked(event) {
-const $lost = $(event.lost)
-$lost.hide()
+const $target = $(event.target)
+$target.hide()
 
 }
-$('#hide_this').click(hideWhenClicked);
+// $('#hide_this').click(hideWhenClicked);
